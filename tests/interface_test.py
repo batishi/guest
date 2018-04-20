4030601c970727ec61ce8cfa7a21c852a428a148
batishi <leisss@163.com>
2018-04-20 09:56:52 +0800

    modify interface print info

--- tests/interface_test.py
+++ tests/interface_test.py
@@ -13,6 +13,7 @@ class AddEventTest(unittest.TestCase):
         payload = {'eid':1,'':'','limit':'','address':"",'start_time':''}
         r = requests.post(self.base_url, data=payload)
         result = r.json()
+        print(1)
         self.assertEqual(result['status'], 10021)
         self.assertEqual(result['message'], 'parameter error')
 
@@ -21,6 +22,7 @@ class AddEventTest(unittest.TestCase):
         payload = {'eid':1,'name':'一加4发布会','limit':2000,'address':"深圳宝体",'start_time':'2017'}
         r = requests.post(self.base_url, data=payload)
         result = r.json()
+        print(2)
         self.assertEqual(result['status'], 10022)
         self.assertEqual(result['message'], 'event id already exists')
 
@@ -29,6 +31,7 @@ class AddEventTest(unittest.TestCase):
         payload = {'eid':11,'name':'一加3手机发布会','limit':2000,'address':"深圳宝体",'start_time':'2017'}
         r = requests.post(self.base_url,data=payload)
         result = r.json()
+        print(3)
         self.assertEqual(result['status'], 10023)
         self.assertEqual(result['message'], 'event name already exists')
 
@@ -37,6 +40,7 @@ class AddEventTest(unittest.TestCase):
         payload = {'eid':11,'name':'一加4手机发布会','limit':2000,'address':"深圳宝体",'start_time':'2017'}
         r = requests.post(self.base_url,data=payload)
         result = r.json()
+        print(4)
         self.assertEqual(result['status'], 10024)
         self.assertIn('start_time format error.', result['message'])
 
@@ -45,6 +49,7 @@ class AddEventTest(unittest.TestCase):
         payload = {'eid':11,'name':'一加4手机发布会','limit':2000,'address':"深圳宝体",'start_time':'2017-05-10 12:00:00'}
         r = requests.post(self.base_url,data=payload)
         result = r.json()
+        print(5)
         self.assertEqual(result['status'], 200)
         self.assertEqual(result['message'], 'add event success')
 
